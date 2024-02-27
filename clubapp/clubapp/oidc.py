5a763dd73de251e0f4d1e2d562a5bfa3b1c6d5c4
fidoriel <49869342+fidoriel@users.noreply.github.com>
2024-02-27 14:31:52 +0000

    fix things and manage permissions

--- clubapp/clubapp/oidc.py
+++ clubapp/clubapp/oidc.py
@@ -7,8 +7,9 @@ from django.http import HttpRequest, HttpResponseRedirect
 from django.utils.http import urlencode
 from josepy.b64 import b64decode
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
+from django.db import transaction
 
-from clubapp.club.models import User
+from clubapp.club.models import User, Membership
 
 
 class ClubOIDCAuthenticationBackend(OIDCAuthenticationBackend):  # type: ignore[misc]
@@ -33,21 +34,34 @@ class ClubOIDCAuthenticationBackend(OIDCAuthenticationBackend):  # type: ignore[
             return "email" in claims
         return True
 
+    @transaction.atomic
     def create_user(self, claims: dict[Any, Any]) -> User:
         user = User.objects.create_user(
             username=claims.get("username", claims.get("preferred_username", claims.get("email"))),
             email=claims["email"],
             first_name=claims.get("firstName", ""),
             last_name=claims.get("lastName", ""),
+            is_staff="staff" in claims.get("group", []),
+            is_clubboat_user=claims.get("clubboat", False),
+            is_boat_owner=claims.get("boat", False),
+            is_superuser="admin" in claims.get("group", []),
+            membership_type=Membership.objects.get(name=claims.get("membership")),
         )
         user.set_unusable_password()
         user.save()
+        print(claims)
         return user
 
     def update_user(self, user: User, claims: dict[Any, Any]) -> User:
         user.email = claims["email"]
         user.first_name = claims.get("firstName", "")
         user.last_name = claims.get("lastName", "")
+        user.is_staff = "staff" in claims.get("group", [])
+        user.is_clubboat_user = claims.get("clubboat", False)
+        user.is_boat_owner = claims.get("boat", False)
+        user.is_superuser = "admin" in claims.get("group", [])
+        user.membership_type = Membership.objects.filter(name=claims.get("membership")).first()
+        print(claims)
         user.save()
 
         if not user.is_active:
@@ -58,7 +72,11 @@ class ClubOIDCAuthenticationBackend(OIDCAuthenticationBackend):  # type: ignore[
 
 def provider_logout(request: "HttpRequest") -> "str":
     redirect_url = request.build_absolute_uri("/")
-    return settings.OIDC_OP_LOGOUT_ENDPOINT + "?" + urlencode({"post_logout_redirect_uri": redirect_url, "client_id": settings.OIDC_RP_CLIENT_ID})
+    return (
+        settings.OIDC_OP_LOGOUT_ENDPOINT
+        + "?"
+        + urlencode({"post_logout_redirect_uri": redirect_url, "client_id": settings.OIDC_RP_CLIENT_ID})
+    )
 
 
 def provider_account_settings(request: "HttpRequest") -> "HttpResponseRedirect":
