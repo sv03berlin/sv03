e9adcb4e8151c1e47b6e66499de74736d99f0714
fidoriel <49869342+fidoriel@users.noreply.github.com>
2024-03-02 12:29:12 +0100

    Minor Changes (#49)
    * change to table
    
    * fix more stuff
    
    * add redirect url conf
    
    * small keycloak stuff
    
    * django 5
    
    * update
    
    * add branch name
    
    * more fixes
    
    * update actions
    
    * update
    
    * fix branch name
    
    * working logout
    
    * fix things and manage permissions
    
    * django is running behind a reverse proxy
    
    * format
    
    * cache admins, help and index page
    
    * tab
    
    * precommit

--- clubapp/clubapp/context_processors.py
+++ clubapp/clubapp/context_processors.py
@@ -2,6 +2,18 @@ from typing import Any
 
 from django.conf import settings
 from django.http import HttpRequest
+from clubapp.club.models import User
+from django.db.models import Q
+from django.core.cache import cache
+
+
+def get_admins() -> list[User]:
+    if (cached_admins := cache.get("admins")) is None:
+        admins = list(User.objects.filter(Q(is_staff=True) | Q(is_superuser=True)))
+        cache.set("admins", admins, 600)
+        return admins
+    else:
+        return cached_admins  # type: ignore[no-any-return]
 
 
 def club_processor(request: HttpRequest) -> dict[str, Any]:
@@ -13,4 +25,5 @@ def club_processor(request: HttpRequest) -> dict[str, Any]:
         "club_imprint": settings.CLUB_IMPRINT,
         "enable_oidc_login": settings.ENABLE_OIDC_LOGIN,
         "enable_django_login": settings.ENABLE_DJANGO_LOGIN,
+        "staff": get_admins(),
     }
