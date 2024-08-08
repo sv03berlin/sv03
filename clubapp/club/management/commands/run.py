066375f5c7e002c653d3c2f28c2bbfbcedd48236
fidoriel <49869342+fidoriel@users.noreply.github.com>
2024-08-08 20:30:16 +0200

    Update run.py to use localhost instead of 0.0.0.0

--- clubapp/club/management/commands/run.py
+++ clubapp/club/management/commands/run.py
@@ -6,9 +6,6 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'Execute "runserver 0.0.0.0:8000"'
-
     def handle(self, *args: Any, **options: Any) -> None:
-        self.stdout.write('Executing "manage.py runserver 0.0.0.0:8000"')
-        sys.argv = ["manage.py", "runserver", "0.0.0.0:8000"]
+        sys.argv = ["manage.py", "runserver", "127.0.0.1:8000"]
         execute_from_command_line(sys.argv)
