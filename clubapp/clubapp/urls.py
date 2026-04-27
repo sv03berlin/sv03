8da9a28da7c60ce7ac1d4f9f7c7866d9a0d61bde
fidoriel <49869342+fidoriel@users.noreply.github.com>
2026-04-27 09:37:30 +0100

    remove refund

--- clubapp/clubapp/urls.py
+++ clubapp/clubapp/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("settings/", views.user_settings, name="settings"),
     path("health/", views.health, name="settings"),
-    path("refundflow/", include("clubapp.refundflow.urls")),
     path("clubwork/", include("clubapp.clubwork.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("reservations/", include("clubapp.reservationflow.urls")),
