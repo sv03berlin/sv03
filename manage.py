4b09a0659a6da31f504d7fbe3f0603c8176c8c0b
fidoriel <49869342+fidoriel@users.noreply.github.com>
2026-05-20 21:08:44 +0200

    fix ruff tz issue

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ def main() -> None:
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clubapp.clubapp.settings")
     try:
-        from django.core.management import execute_from_command_line  # noqa: PLC0415
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         msg = (
             "Couldn't import Django. Are you sure it's installed and "
